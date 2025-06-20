af33246a87c39e5a9e8031da664f3aa96e7d6ae0
MinhKhanh188 <jokhanh188@gmail.com>
2025-06-20 22:20:26 +0700

    20/6/2026: dev test cross origin

--- front-end/src/socket.js
+++ front-end/src/socket.js
@@ -1,14 +1,13 @@
 // front-end/src/socket.js
 import { io } from 'socket.io-client';
 
-const socketUrl =
-  import.meta.env.MODE === 'development'
-    ? import.meta.env.VITE_BACKEND_LOCAL_URL
-    : import.meta.env.VITE_BACKEND_PRODUCTION_URL_DEV;
-
-const socket = io(socketUrl, {
+const socket = io(
+  //import.meta.env.VITE_BACKEND_LOCAL_URL
+  import.meta.env.VITE_BACKEND_PRODUCTION_URL_DEV
+  , {
   withCredentials: true,
   transports: ['websocket'],
 });
 
 export default socket;
+
